46cd508e22a7682c3b0828ad7944c7d667f0197c
yeonw00 <mrk8056@gmail.com>
2025-08-12 22:47:01 +0900

    네이버로 로그인하는 버튼 삽입

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "../.css";
 import googleLogo from "../image/signin-assets/google_logo.png";
+import naverLogo from "../image/signin-assets/naver_logo.png";
 
 function Home() {
     const navigate = useNavigate();
@@ -15,6 +16,16 @@ function Home() {
         }
     };
 
+    const handleNaverLogin = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/api/auth/naver/login");
+            const loginUrl = await response.text();
+            window.location.href = loginUrl;
+        } catch (error) {
+            console.error("네이버 로그인 URL 요청 실패:", error);
+        }
+    };
+
     return (
         <div className="home">
             <h1>News Summary</h1>
@@ -26,8 +37,11 @@ function Home() {
             </div>
             <br />
             <br />
-            <button className="google-login-btn" onClick={handleGoogleLogin}>
+            <button className="social-login-btn" onClick={handleGoogleLogin}>
                 <img src={googleLogo} alt="Google Logo" />
+            </button>
+            <button className="social-login-btn" onClick={handleNaverLogin}>
+                <img src={naverLogo} alt="Naver Logo" />
             </button>
         </div>
     );
